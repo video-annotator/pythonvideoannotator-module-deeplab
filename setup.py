9643e4bd055139df6d916eff364edc379025d2ac
Manuel Manso <manel_manso@hotmail.com>
2019-04-17 15:52:32 +0100

    Prepare to pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+setup(
+	name='Python video annotator - module - deeplab',
+	version=0.0,
+	description="""""",
+	author=['Manuel Manso'],
+	author_email='manel_manso@hotmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-deeplab',
+	packages=find_packages(),
+)

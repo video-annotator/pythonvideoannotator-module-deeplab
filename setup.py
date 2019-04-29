2b18369fce09f36e69d1731a9720da78d8a7d11b
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-04-29 16:55:42 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ import re
 
 setup(
 	name='Python video annotator - module - deeplab',
-	version=0.4,
+	version=0.5,
 	description="""""",
 	author=['Manuel Manso'],
 	author_email='manel_manso@hotmail.com',
 	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-deeplab',
-	packages=find_packages()
+	packages=find_packages(),
+	install_requires=['pyyaml']
 )

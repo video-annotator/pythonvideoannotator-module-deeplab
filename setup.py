bfb5598be297552de1ddf3e5f8d33dfa43897162
Manuel Manso <manel_manso@hotmail.com>
2019-05-09 14:06:12 +0100

    Add README to Pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@
 from setuptools import setup, find_packages
 import re
 
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
 setup(
 	name='Python video annotator - module - deeplab',
-	version=0.6,
+	version=0.7,
 	description="""""",
 	author=['Manuel Manso'],
 	author_email='manel_manso@hotmail.com',
 	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-deeplab',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
+
 	packages=find_packages(),
 	install_requires=['pyyaml']
 )

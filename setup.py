03f6aab8c1b55d6c14878096250429561fe2f1d4
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 13:58:05 +0100

    Update versioning system

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ import re
 with open('README.md', 'r') as fd:
     long_description = fd.read()
 
+import os, re;
+with open(os.path.join(os.path.dirname(__file__), 'pythonvideoannotator_module_deeplab','__init__.py')) as fd:
+	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+
 setup(
 	name='Python video annotator - module - deeplab',
-	version="0.901",
+	version=version,
 	description="""""",
 	author=['Manuel Manso'],
 	author_email='manel_manso@hotmail.com',
